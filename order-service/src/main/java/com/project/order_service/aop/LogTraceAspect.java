96b968f949a22e908bdd79f6599a707c0d042b3e
Seungil <seungilbang@khu.ac.kr>
2025-02-02 23:48:58 +0900

    order-service -> AOP 로그 트레이싱 추가

---
+++ order-service/src/main/java/com/project/order_service/aop/LogTraceAspect.java
@@ -0,0 +1,32 @@
+package com.project.order_service.aop;
+
+import brave.Span;
+import brave.Tracer;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogTraceAspect {
+
+    private final Tracer tracer;
+
+    public LogTraceAspect(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Around("execution(* com.project.order_service.repository.*Repository.*(..))")
+    public Object traceRepositoryLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        Span newSpan = tracer.nextSpan().name(joinPoint.getSignature().toString()).start();
+        try (Tracer.SpanInScope ws = tracer.withSpanInScope(newSpan.start())) {
+            return joinPoint.proceed();
+        } catch (Throwable e) {
+            newSpan.error(e);
+            throw e;
+        } finally {
+            newSpan.finish();
+        }
+    }
+}
